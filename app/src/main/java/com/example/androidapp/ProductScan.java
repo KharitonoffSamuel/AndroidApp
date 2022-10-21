03debaca68b709abf13878783018a98962908705
Hugo <hugo.kantardjian@edu.esiee.fr>
2022-10-21 20:37:25 +0200

    Ajout activités recherche et scan (layouts seulement)

--- app/src/main/java/com/example/androidapp/ProductScan.java
+++ app/src/main/java/com/example/androidapp/ProductScan.java
@@ -2,15 +2,13 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class ProductSearch extends AppCompatActivity {
+public class ProductScan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_search);
+        setContentView(R.layout.activity_product_scan);
     }
 }
