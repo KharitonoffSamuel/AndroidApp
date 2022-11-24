75fd20f678c09ef0fc19fc1c5ecbf7ad656d18c2
Hugo <hugo.kantardjian@edu.esiee.fr>
2022-11-24 15:37:28 +0100

    24/11 : OUIIII, la liste avec les cases à cocher pour sélectionner les emballages fonctionne !

--- app/src/main/java/com/example/androidapp/RVAdapter.java
+++ app/src/main/java/com/example/androidapp/RVAdapter.java
@@ -1,6 +1,7 @@
 package com.example.androidapp;
 
 
+import android.content.Context;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,14 +10,30 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ItemViewHolder> {
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ItemViewHolder>{
 
-    List<String> dataList;
+    View view;
+    ArrayList<String> dataList;
+    Context context;
+    CheckboxListener checkboxListener;
+    ArrayList<String> dataList_0 = new ArrayList<>();
+
+    public RVAdapter(ArrayList<String> dataList, Context context, CheckboxListener checkboxListener) {
+        this.dataList = dataList;
+        this.context = context;
+        this.checkboxListener = checkboxListener;
+    }
+
+    public View getView() {
+        return view;
+    }
 
     public class ItemViewHolder extends RecyclerView.ViewHolder {
         CheckBox checkBox;
@@ -27,23 +44,33 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ItemViewHolder> {
         }
     }
 
-    public RVAdapter(List<String> dataList) {
-        this.dataList = dataList;
-    }
-
     @Override
     public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_list_item, parent,
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_list_item, parent,
                 false);
 
         return new ItemViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ItemViewHolder holder, int position) {
-        holder.checkBox.setText(dataList.get(position));
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
         //https://www.youtube.com/watch?v=5YFPkFaLcIo
+        if(dataList != null && dataList.size()>0){
+            holder.checkBox.setText(dataList.get(position));
+            holder.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (holder.checkBox.isChecked()){
+                        dataList_0.add(dataList.get(position));
+                    }
+                    else{
+                        dataList_0.remove(dataList.get(position));
+                    }
+                    checkboxListener.onCheckboxChange(dataList_0);
+                }
+            });
+        }
     }
 
     @Override
